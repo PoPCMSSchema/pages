70868665d5e6cf70ca9284c579e87b72a1be9583
Leo <losoviz@gmail.com>
2019-10-22 23:16:31 +0800

    Moved `getPagePath` under Pages

---
+++ src/Route/PathUtils.php
@@ -0,0 +1,24 @@
+<?php
+namespace PoP\Pages\Route;
+
+class PathUtils {
+
+    public static function getPagePath($page_id)
+    {
+        $cmsengineapi = \PoP\Engine\FunctionAPIFactory::getInstance();
+        $cmspagesapi = \PoP\Pages\FunctionAPIFactory::getInstance();
+
+        // Generate the page path. Eg: http://mesym.com/events/past/ will render events/past
+        $permalink = $cmspagesapi->getPageURL($page_id);
+
+        $domain = $cmsengineapi->getHomeURL();
+
+        // Remove the domain from the permalink => page path
+        $page_path = substr($permalink, strlen($domain));
+
+        // Remove the first and last '/'
+        $page_path = trim($page_path, '/');
+
+        return $page_path;
+    }
+}
