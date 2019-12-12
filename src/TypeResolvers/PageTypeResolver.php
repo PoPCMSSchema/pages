4111993eb5ffb9d75a609033e01e44ee9610f95e
Leo <losoviz@gmail.com>
2019-12-12 17:45:07 +0800

    Added `getSchemaTypeDescription`

--- src/TypeResolvers/PageTypeResolver.php
+++ src/TypeResolvers/PageTypeResolver.php
@@ -1,8 +1,9 @@
 <?php
 namespace PoP\Pages\TypeResolvers;
 
-use PoP\ComponentModel\TypeResolvers\AbstractTypeResolver;
 use PoP\Pages\TypeDataLoaders\PageTypeDataLoader;
+use PoP\Translation\Facades\TranslationAPIFacade;
+use PoP\ComponentModel\TypeResolvers\AbstractTypeResolver;
 
 class PageTypeResolver extends AbstractTypeResolver
 {
@@ -11,6 +12,12 @@ class PageTypeResolver extends AbstractTypeResolver
     public function getTypeName(): string
     {
         return self::NAME;
+    }
+
+    public function getSchemaTypeDescription(): ?string
+    {
+        $translationAPI = TranslationAPIFacade::getInstance();
+        return $translationAPI->__('Representation of a page', 'pages');
     }
 
     public function getId($resultItem)
