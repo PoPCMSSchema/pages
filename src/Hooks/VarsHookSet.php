f2e5c2dbfa9c6afb15bc57dfb675796a877e8602
leoloso <leo@getpop.org>
2022-01-14 06:57:06 +0000

    Merge pull request #1356 from leoloso/Create-root-wp,-move-there-TranslationWP,-HooksWP-and-RoutingWP
    Create RootWP, move there TranslationWP, HooksWP and RoutingWP

--- src/Hooks/VarsHookSet.php
+++ src/Hooks/VarsHookSet.php
@@ -6,7 +6,7 @@ namespace PoPSchema\Pages\Hooks;
 
 use PoP\Root\App;
 use PoP\ComponentModel\ModelInstance\ModelInstance;
-use PoP\BasicService\AbstractHookSet;
+use PoP\Root\Hooks\AbstractHookSet;
 use PoPSchema\Pages\Constants\ModelInstanceComponentTypes;
 use PoPSchema\Pages\Routing\RouteNatures;
 
@@ -14,7 +14,7 @@ class VarsHookSet extends AbstractHookSet
 {
     protected function init(): void
     {
-        $this->getHooksAPI()->addFilter(
+        App::addFilter(
             ModelInstance::HOOK_COMPONENTS_RESULT,
             array($this, 'getModelInstanceComponentsFromAppState')
         );
@@ -24,7 +24,7 @@ class VarsHookSet extends AbstractHookSet
     {
         switch (App::getState('nature')) {
             case RouteNatures::PAGE:
-                $component_types = $this->getHooksAPI()->applyFilters(
+                $component_types = App::applyFilters(
                     '\PoPSchema\Pages\ModelInstanceProcessor_Utils:components_from_vars:type:page',
                     []
                 );
