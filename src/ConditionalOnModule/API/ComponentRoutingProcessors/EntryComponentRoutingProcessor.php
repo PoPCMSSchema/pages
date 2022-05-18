606dcad1605b4079fe9abbb14fef8dc135f81ba3
leoloso <leo@getpop.org>
2022-05-18 06:39:35 +0000

    Merge pull request #1590 from leoloso/Rename-ModuleRouting-to-ComponentRouting
    Rename ModuleRouting to ComponentRouting

--- src/ConditionalOnModule/API/ComponentRoutingProcessors/EntryComponentRoutingProcessor.php
+++ src/ConditionalOnModule/API/ComponentRoutingProcessors/EntryComponentRoutingProcessor.php
@@ -2,24 +2,24 @@
 
 declare(strict_types=1);
 
-namespace PoPCMSSchema\Pages\ConditionalOnModule\API\RouteModuleProcessors;
+namespace PoPCMSSchema\Pages\ConditionalOnModule\API\ComponentRoutingProcessors;
 
 use PoPAPI\API\Response\Schemes as APISchemes;
-use PoP\ModuleRouting\AbstractEntryRouteModuleProcessor;
+use PoP\ComponentRouting\AbstractEntryComponentRoutingProcessor;
 use PoPCMSSchema\Pages\ModuleProcessors\FieldDataloadModuleProcessor;
 use PoPCMSSchema\Pages\Routing\RequestNature;
 
-class EntryRouteModuleProcessor extends AbstractEntryRouteModuleProcessor
+class EntryComponentRoutingProcessor extends AbstractEntryComponentRoutingProcessor
 {
     /**
      * @return array<string, array<array>>
      */
-    public function getModulesVarsPropertiesByNature(): array
+    public function getStatePropertiesToSelectComponentByNature(): array
     {
         $ret = array();
 
         $ret[RequestNature::PAGE][] = [
-            'module' => [FieldDataloadModuleProcessor::class, FieldDataloadModuleProcessor::MODULE_DATALOAD_RELATIONALFIELDS_PAGE],
+            'component' => [FieldDataloadModuleProcessor::class, FieldDataloadModuleProcessor::MODULE_DATALOAD_RELATIONALFIELDS_PAGE],
             'conditions' => [
                 'scheme' => APISchemes::API,
             ],
--- src/ConditionalOnModule/RESTAPI/ComponentRoutingProcessors/EntryComponentRoutingProcessor.php
+++ src/ConditionalOnModule/RESTAPI/ComponentRoutingProcessors/EntryComponentRoutingProcessor.php
@@ -2,15 +2,15 @@
 
 declare(strict_types=1);
 
-namespace PoPCMSSchema\Pages\ConditionalOnModule\RESTAPI\RouteModuleProcessors;
+namespace PoPCMSSchema\Pages\ConditionalOnModule\RESTAPI\ComponentRoutingProcessors;
 
 use PoP\Root\App;
 use PoPAPI\API\Response\Schemes as APISchemes;
-use PoPAPI\RESTAPI\RouteModuleProcessors\AbstractRESTEntryRouteModuleProcessor;
+use PoPAPI\RESTAPI\ComponentRoutingProcessors\AbstractRESTEntryComponentRoutingProcessor;
 use PoPCMSSchema\Pages\ModuleProcessors\FieldDataloadModuleProcessor;
 use PoPCMSSchema\Pages\Routing\RequestNature;
 
-class EntryRouteModuleProcessor extends AbstractRESTEntryRouteModuleProcessor
+class EntryComponentRoutingProcessor extends AbstractRESTEntryComponentRoutingProcessor
 {
     protected function getInitialRESTFields(): string
     {
@@ -20,12 +20,12 @@ class EntryRouteModuleProcessor extends AbstractRESTEntryRouteModuleProcessor
     /**
      * @return array<string, array<array>>
      */
-    public function getModulesVarsPropertiesByNature(): array
+    public function getStatePropertiesToSelectComponentByNature(): array
     {
         $ret = array();
 
         $ret[RequestNature::PAGE][] = [
-            'module' => [
+            'component' => [
                 FieldDataloadModuleProcessor::class,
                 FieldDataloadModuleProcessor::MODULE_DATALOAD_RELATIONALFIELDS_PAGE,
                 [
